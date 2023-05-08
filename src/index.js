9c0946a01efec611e5c70418d27b29fb8fc252dd
Mariana M.C <marianamoioli@gmail.com>
2023-05-08 15:12:34 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Login from './containers/Login'
+import GlobalStyles from './styles/globalStyles'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <>
+   <Login/>
+   <GlobalStyles />
+  </>
+ 
+)
